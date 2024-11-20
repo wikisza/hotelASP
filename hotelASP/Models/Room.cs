0f327d87df4e67674960db3104708efd46ee90ec
Wiki <wiki@example.com>
2024-11-20 11:16:51 +0100

    changed room, added more edit options

--- hotelASP/Models/Room.cs
+++ hotelASP/Models/Room.cs
@@ -6,7 +6,7 @@ namespace hotelASP.Models
     {
         [Key]
         public int Id_room { get; set; }
-        public float Price { get; set; }
+        public required float Price { get; set; }
         public required string Description { get; set; }
         public int Is_taken { get; set; }
     }
